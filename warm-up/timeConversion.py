93449c247b6b0dc3285bc6680483b571be0621aa
hoanngocle <hoanngocle.93@gmail.com>
2022-12-07 17:16:53 +0700

    Refactor code

--- warm-up/timeConversion.py
+++ warm-up/timeConversion.py
@@ -4,10 +4,9 @@ def timeConversion(string):
     string = string.split(":")
     hour = "00"
     if "AM" in string[2]:
-        if string[0] == "12": 
-            hour = "00"
-        else:
+        if string[0] != "12": 
             hour = string[0]
+            
         return hour + ":" + string[1] + ":" + string[2].replace("AM", "")
     elif "PM" in string[2]:
         if string[0] == "12": 
@@ -16,7 +15,7 @@ def timeConversion(string):
             hour = str(int(string[0]) + 12)
         return hour + ":" + string[1] + ":" + string[2].replace("PM", "")
 
-string = "06:40:03AM"
+string = "08:40:03AM"
 # Call function
 output = timeConversion(string)
 
